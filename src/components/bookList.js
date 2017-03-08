e4e16eb0a182676955b31639b73103680f3d815f
Matheus Carmo <mematheuslc@gmail.com>
2017-03-08 03:34:49 -0300

    feat(books): Add bookList component

---
+++ src/components/bookList.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { search } from '../actions/index';
+import { Book } from '../components/book';
+
+/**
+ * @class
+ * @name BookList
+ * @description Book List component
+ */
+class BookList extends Component {
+  componentWillMount() {
+    this.props.search();
+  }
+
+  renderBooks() {
+    return this.props.books.map((book) => {
+      return (
+        <div key={book.id}>
+          <Book book={book} />
+        </div>
+      )
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        { this.renderBooks() }
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return { books: state.books.books };
+}
+
+export default connect(mapStateToProps, { search })(BookList)
