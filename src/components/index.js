ed4c04196e5556e26739fd71f91a67f1e126c809
Matheus Carmo <mematheuslc@gmail.com>
2017-03-08 03:08:07 -0300

    feat(index): Application component entrypoint

---
+++ src/components/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+/**
+ * @class
+ * @name Index
+ * @description Application entrypoint
+ */
+export default class Index extends Component {
+  constructor(props) {
+
+    super(props);
+  }
+
+  render() {
+    return (
+     <div>Hello World :P</div>
+    )
+  }
+}
