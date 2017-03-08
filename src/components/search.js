a4ff5fd55c4bd3e3e0ac65f7300271ea2a7b3544
Matheus Carmo <mematheuslc@gmail.com>
2017-03-08 03:46:05 -0300

    feat(search): Trigger search event

--- src/components/search.js
+++ src/components/search.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { search } from '../actions/index';
 
 /**
  * @class
  * @name Search
  * @description Search bar
  */
-export default class Search extends Component {
+class Search extends Component {
   constructor(props) {
     super(props);
+    this.search = this.search.bind(this);
+  }
+
+  search(event) {
+    this.props.search(event.target.value);
   }
 
   render() {
     return (
-      <input className="search__input" type="search" />
+      <input className="search__input"
+             type="search"
+             onChange={this.search} />
     )
   }
 }
+
+function mapStateToProps(state) {
+  return { search: state.search };
+}
+
+export default connect(mapStateToProps, { search })(Search)
